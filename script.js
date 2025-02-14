26b2010531d3420cadc93211471c379ae21f1787
MugiCat21 <jake23032019@gmail.com>
2025-02-14 08:22:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,24 @@
     scrolling="no" 
       frameborder="no" 
         allow="autoplay" 
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2034357168&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/mugi-972192703" title="Mugi" target="_blank" style="color: #cccccc; text-decoration: none;">Mugi</a> · <a href="https://soundcloud.com/mugi-972192703/yoshis-island-ost-track-01" title="Yoshi&#x27;s Island OST - Track 01 - Story Music Box - Nerd Lab OG" target="_blank" style="color: #cccccc; text-decoration: none;">Yoshi&#x27;s Island OST - Track 01 - Story Music Box - Nerd Lab OG</a></div>
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2034357168&
+            color=%23ff5500&
+            auto_play=true&
+            hide_related=false&
+            show_comments=true&
+            show_user=true&
+            show_reposts=false&
+            show_teaser=true&
+            visual=true">
+            </iframe><div style="font-size: 10px; 
+            color: #cccccc;line-break: 
+              anywhere;word-break: 
+              normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a 
+            href="https://soundcloud.com/mugi-972192703" 
+              title="Mugi" 
+                target="_blank" 
+                  style="color: #cccccc; 
+
+
+text-decoration: none;">Mugi</a> · <a href="https://soundcloud.com/mugi-972192703/yoshis-island-ost-track-01" title="Yoshi&#x27;s Island OST - Track 01 - Story Music Box - Nerd Lab OG" target="_blank" style="color: #cccccc; text-decoration: none;">Yoshi&#x27;s Island OST - Track 01 - Story Music Box - Nerd Lab OG</a></div>
   
